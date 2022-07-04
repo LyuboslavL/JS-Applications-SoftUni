be12e3cc2207e734bdf163711ca0d09fa9b41c7c
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2022-07-04 17:45:59 +0300

    Added user authentication

--- 04 - Single Page Applications/Exercise/02.Movies/src/details.js
+++ 04 - Single Page Applications/Exercise/02.Movies/src/details.js
@@ -7,19 +7,42 @@ async function getMovieById(movieId) {
 };
 
 function createMovieCard(movie) {
-    const element =
-        e('div', { className: 'container' },
-            e('div', { className: 'row bg-light text-dark' },
-                e('h1', {}, `Movie title: ${movie.title}`),
-                e('div', { className: 'col-md-8' },
-                    e('img', { className: 'img-thumbnail', src: movie.img, alt: 'Movie' })),
-                e('div', { className: 'col-md-4 text-center' },
-                    e('h3', { className: 'my-3 ' }, 'Movie Description'),
-                    e('p', {}, movie.description),
-                    e('a', { className: 'btn btn-danger' }, 'Delete'),
-                    e('a', { className: 'btn btn-warning' }, 'Edit'),
-                    e('a', { className: 'btn btn-primary' }, 'Like'),
-                    e('span', { className: 'enrolled-span' }, 'Liked 1'))));
+    const userId = sessionStorage.getItem('userId');
+    let element;
+
+    if (userId !== null) {
+        if (userId == movie._ownerId) {
+            element =
+                e('div', { className: 'container' },
+                    e('div', { className: 'row bg-light text-dark' },
+                        e('h1', {}, `Movie title: ${movie.title}`),
+                        e('div', { className: 'col-md-8' },
+                            e('img', { className: 'img-thumbnail', src: movie.img, alt: 'Movie' })),
+                        e('div', { className: 'col-md-4 text-center' },
+                            e('h3', { className: 'my-3 ' }, 'Movie Description'),
+                            e('p', {}, movie.description),
+                            e('a', { className: 'btn btn-danger' }, 'Delete'),
+                            e('a', { className: 'btn btn-warning' }, 'Edit'),
+                            // e('a', { className: 'btn btn-primary' }, 'Like'),
+                            e('span', { className: 'enrolled-span' }, 'Liked 1')
+                        )));
+        } else {
+            element =
+                e('div', { className: 'container' },
+                    e('div', { className: 'row bg-light text-dark' },
+                        e('h1', {}, `Movie title: ${movie.title}`),
+                        e('div', { className: 'col-md-8' },
+                            e('img', { className: 'img-thumbnail', src: movie.img, alt: 'Movie' })),
+                        e('div', { className: 'col-md-4 text-center' },
+                            e('h3', { className: 'my-3 ' }, 'Movie Description'),
+                            e('p', {}, movie.description),
+                            // e('a', { className: 'btn btn-danger' }, 'Delete'),
+                            // e('a', { className: 'btn btn-warning' }, 'Edit'),
+                            e('a', { className: 'btn btn-primary' }, 'Like'),
+                            e('span', { className: 'enrolled-span' }, 'Liked 1')
+                        )));
+        }
+    }
 
     return element;
 }
