20cc5c4f8ceb000566d32cc799cbf561b3b4a83c
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2022-07-12 16:37:29 +0300

    Added page changing

--- 05 - Architecture and Testing/Lab/Playwright/testingPlaywright.js
+++ 05 - Architecture and Testing/Lab/Playwright/testingPlaywright.js
@@ -1,10 +1,12 @@
 const { chromium } = require('playwright-chromium');
 
 (async () => {
-    const browser = await chromium.launch();
+    const browser = await chromium.launch({ headless: false, slowMo: 200 });
     const page = await browser.newPage();
-    
+
     await page.goto('http://softuni.bg/');
-    await page.screenshot({path: 'softuni-screenshot.png'});
+    await page.screenshot({ path: 'softuni-screenshot.png' });
+    await page.click('text=ПРЕПОДАВАТЕЛИ');
+    await page.screenshot({ path: 'teachers-screenshot.png' });
     await browser.close();
 })();
